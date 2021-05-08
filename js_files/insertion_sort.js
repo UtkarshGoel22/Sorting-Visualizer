dc2edc890c45e90ae3ed99f5e236c6e1ddcc507f
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2021-05-08 14:32:29 +0530

    Implement insertion sort

---
+++ js_files/insertion_sort.js
@@ -0,0 +1,26 @@
+async function insertionSort() {
+    console.log("Inside insertionSort()");
+    const arr = document.querySelectorAll(".bar");
+    for(let i = 1; i < arr.length; i++) {
+        arr[i].style.background = "blue";
+        let key = arr[i].style.height;
+        let j = i-1;
+        while(j >= 0 && parseInt(arr[j].style.height) > parseInt(key)) {
+            arr[j].style.background = "blue";
+            arr[j+1].style.height = arr[j].style.height;
+            j--; 
+            await waitForMe(delay);
+            for(let k = i; k >= 0; k--) {
+                arr[k].style.background = "green";
+            }
+        }
+        arr[j+1].style.height = key;
+        arr[i].style.background = "green";
+    }
+}
+
+const insertionSortBtn = document.querySelector(".insertionSort");
+
+insertionSortBtn.addEventListener("click", async ()=> {
+    await insertionSort();
+})
