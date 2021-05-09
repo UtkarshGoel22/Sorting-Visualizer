cb2f9069ee1bd32e10a1d497790f5eb1c377fdf2
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2021-05-09 12:24:44 +0530

    Add comments in bubble_sort.js

--- js_files/bubble_sort.js
+++ js_files/bubble_sort.js
@@ -3,17 +3,21 @@ async function bubbleSort() {
     const arr = document.querySelectorAll(".bar");
     for(let i = 0; i < arr.length-1; i++) {
         for(let j = 0; j < arr.length-i-1; j++) {
+            // change color of jth & (j+1)th bar to blue
             arr[j].style.background = "blue";
             arr[j+1].style.background = "blue";
             if(parseInt(arr[j].style.height) > parseInt(arr[j+1].style.height)) {
                 await waitForMe(delay);
                 swap(arr[j], arr[j+1]);
             }
+            // change color of jth & (j+1)th bar back to original color
             arr[j].style.background = "cyan";
             arr[j+1].style.background = "cyan";
         }
+        // change color of sorted element to green
         arr[arr.length-i-1].style.background = "green";
     }
+    // change color of 0th bar to green
     arr[0].style.background = "green";
 }
 
